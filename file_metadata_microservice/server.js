367c3bcee29576080078d31dd2ac4f852acc6634
mowabanga <itsmundia@gmail.com>
2024-03-21 18:44:02 +0300

    Added new files

--- file_metadata_microservice/server.js
+++ file_metadata_microservice/server.js
@@ -1,39 +1,39 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
+require("dotenv").config();
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
 // require and use "multer"...
-var multer = require('multer');
-var upload = multer({ dest: 'uploads/' });
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
   try {
     res.json({
-      "name": req.file.originalname,
-      "type": req.file.mimetype,
-      "size": req.file.size
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
     });
   } catch (err) {
     res.send(400);
   }
 });
 
-app.get('/hello', function (req, res) {
+app.get("/hello", function (req, res) {
   res.json({ greetings: "Hello, API" });
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+let portListener = app.listen(process.env.PORT || 3000, function () {
+  console.log(`Node.js listening on port ${portListener.address().port}`);
 });
